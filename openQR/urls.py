2c73a15a7bff3529e2dd89a0f3b14cb217f0e6e9
msterhuj <gabin.lanore@gmail.com>
2023-10-18 11:23:06 +0200

    feat: add dashboard

--- openQR/urls.py
+++ openQR/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 from front.views import index as front_index
+from dashboard.urls import urlpatterns as dashboard_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", front_index, name="front_index"),
+    path("dashboard/", include(dashboard_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
