e51d65dc48650552b89a19255d9a5b28b815af3d
msterhuj <gabin.lanore@gmail.com>
2023-10-19 15:32:29 +0200

    feat: add register

--- dashboard/auth_forms.py
+++ dashboard/auth_forms.py
@@ -4,3 +4,10 @@ from django import forms
 class LoginForm(forms.Form):
     inputUsername = forms.CharField(label="Your name", max_length=100, widget=forms.TextInput)
     inputPassword = forms.CharField(label="Your password", max_length=100, widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    inputUserName = forms.CharField(label="Your name", max_length=100, widget=forms.TextInput)
+    inputEmail = forms.EmailField(label="Your email", max_length=100, widget=forms.EmailInput)
+    inputPassword = forms.CharField(label="Your password", max_length=100, widget=forms.PasswordInput)
+    inputPasswordConfirm = forms.CharField(label="Your password", max_length=100, widget=forms.PasswordInput)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import authenticate, login
 
-from .auth_forms import LoginForm
+from .auth_forms import LoginForm, RegisterForm
 
 
 def index_view(request):
@@ -36,8 +37,32 @@ def login_view(request):
 
 
 def register_view(request):
-    return render(request, "dashboard/auth/register.html")
 
+    if request.user.is_authenticated:
+        return redirect("dashboard_index")
+
+    if request.method != "POST":
+        return render(request, "dashboard/auth/register.html", {"form": RegisterForm()})
+
+    form = RegisterForm(request.POST)
+    if not form.is_valid():
+        return render(request, "dashboard/auth/register.html", {"form": form})
+
+    username = form.cleaned_data["inputUserName"]
+    email = form.cleaned_data["inputEmail"]
+    password = form.cleaned_data["inputPassword"]
+    password_confirm = form.cleaned_data["inputPasswordConfirm"]
+
+    if password != password_confirm:
+        return render(request, "dashboard/auth/register.html", {"form": form})
+
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+    session_user = authenticate(request, username=username, password=password)
+    login(request, session_user)
+
+    return redirect("dashboard_index")
 
 def logout_view(request):
     auth_logout(request)
